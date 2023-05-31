ffde11646c5dcd32b76b15f62b2d1e941e44b7b6
YuriDLL <fifnarr@gmail.com>
2023-05-31 23:56:13 +0300

    add: full nonogram slover

--- nonogram_slover.py
+++ nonogram_slover.py
@@ -8,77 +8,6 @@ class Mark(IntEnum):
     FILL = -1
 
 
-def fill_row(row: List[int], numbers, reverse=False):
-    if reverse:
-        row.reverse()
-        numbers.reverse()
-    row_i = 0
-    for num_i, number in enumerate(numbers):
-        number_fill = num_i if not reverse else len(numbers) - 1 - num_i
-        for dot in range(number):
-            while row[row_i] == Mark.CROSS:
-                row_i += 1
-            row[row_i] = number_fill
-            row_i += 1
-        while row_i < len(row) and row[row_i] == Mark.FILL:
-            row[row_i] = number_fill
-            row[row_i - number] = Mark.EMPTY
-            row_i += 1
-        row_i += 1
-    if row_i < len(row):
-        for i in range(row_i, len(row)):
-            if row[i] == Mark.FILL:
-                last_num_fill = len(numbers) - 1
-                for i, cell in enumerate(row):
-                    if cell == last_num_fill:
-                        row[i] = Mark.EMPTY
-                for insert_i in range(i - numbers[-1] + 1, i + 1):
-                    row[insert_i] = last_num_fill
-    if reverse:
-        row.reverse()
-        numbers.reverse()
-
-
-def unite_rows(left_row, rigth_row, numbers):
-    row_len = len(left_row)
-    row = [Mark.EMPTY for cell in range(row_len)]
-    complite_numbers = [0 for cell in range(len(numbers))]
-    for rigth_cell, left_cell, col in zip(left_row, rigth_row, range(row_len)):
-        assert rigth_cell != Mark.FILL
-        assert left_cell != Mark.FILL
-        if rigth_cell == left_cell and rigth_cell >= 0:
-            complite_numbers[rigth_cell] += 1
-            row[col] = Mark.FILL
-    full_complite = True
-    for complite_number, number, i in \
-            zip(complite_numbers, numbers, range(len(numbers))):
-        if complite_number == number:
-            num_start_i = left_row.index(i)
-            if num_start_i != 0:
-                row[num_start_i - 1] = Mark.CROSS
-            num_end_i = num_start_i + number
-            if num_end_i != row_len:
-                row[num_end_i] = Mark.CROSS
-        else:
-            full_complite = False
-    if full_complite:
-        row = [Mark.FILL if cell == Mark.FILL else Mark.CROSS for cell in row]
-    return row, full_complite
-
-
-def fill_side(field: List[list], numbers):
-    field_complite = True
-    for row, row_numbes in enumerate(numbers):
-        left_row = field[row].copy()
-        fill_row(left_row, row_numbes)
-        rigth_row = field[row].copy()
-        fill_row(rigth_row, row_numbes, reverse=True)
-        pass
-        field[row], row_colplite = unite_rows(left_row, rigth_row, row_numbes)
-        field_complite &= row_colplite
-    return field_complite
-
-
 def append_number(row: list, number: int, start_index: int) -> Optional[list]:
     if start_index + number > len(row):
         return None
@@ -143,10 +72,40 @@ def fill_row_brute(row: list, numbers: list) -> list:
     return new_row
 
 
-def slove(numbers):
+def is_complete_row(row: list) -> bool:
+    for cell in row:
+        if cell == Mark.EMPTY:
+            return False
+    return True
+
+
+def fill_side(field: List[list], numbers) -> bool:
+    field_complite = True
+    for i in range(len(field)):
+        if is_complete_row(field[i]):
+            continue
+        field[i] = fill_row_brute(field[i], numbers[i])
+        field_complite = False
+    return field_complite
+
+
+def rotate_field(field: List[list]) -> list:
+    rotated_field = []
+    for i in range(len(field[0])):
+        row = []
+        for j in range(len(field)):
+            row.append(field[j][i])
+        rotated_field.append(row)
+    return rotated_field
+
+
+def slove(numbers) -> list:
     assert len(numbers) == 2
-    field = [[Mark.EMPTY for row in range(len(numbers[0]))]
-             for column in range(len(numbers[1]))]
+    field = [[Mark.EMPTY for row in range(len(numbers[1]))] for column in range(len(numbers[0]))]
     sloved = False
     while (not sloved):
         sloved = fill_side(field, numbers[0])
+        field = rotate_field(field)
+        sloved = fill_side(field, numbers[1])
+        field = rotate_field(field)
+    return field
--- tests/test_rows.py
+++ tests/test_rows.py
@@ -1,8 +1,31 @@
-from nonogram_slover import fill_row_brute, Mark
+from nonogram_slover import fill_row_brute, Mark, slove
 
 
 def assert_row(row, numbers, ans):
     assert fill_row_brute(row, numbers) == ans
+
+
+def convert_to_marks(str_field: list) -> list:
+    mark_field = []
+    for str_row in str_field:
+        mark_row = []
+        for sym in str_row:
+            if sym == '◼':
+                mark_row.append(Mark.FILL)
+            else:
+                mark_row.append(Mark.CROSS)
+        mark_field.append(mark_row)
+    return mark_field
+
+
+def marks_to_str(mark_field: list) -> list:
+    str_field = []
+    for mark_row in mark_field:
+        str_row = ''
+        for sym in mark_row:
+            str_row += '◼' if sym == Mark.FILL else '◻'
+        str_field.append(str_row)
+    return str_field
 
 
 def test_unrecognized():
@@ -53,3 +76,130 @@ def test_fill_classification():
         ans=[Mark.CROSS, Mark.EMPTY, Mark.FILL, Mark.EMPTY, Mark.CROSS,
              Mark.CROSS, Mark.CROSS, Mark.FILL, Mark.FILL, Mark.FILL]
     )
+
+
+def test_slove_easy():
+    rule = [
+        [
+            [10],
+            [1, 1],
+            [1, 1, 1],
+            [1, 3, 1],
+            [1, 1],
+            [1, 3, 1],
+            [1, 1],
+            [1, 3, 1],
+            [1, 1],
+            [10]
+        ],
+        [
+            [10],
+            [1, 1],
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1],
+            [1, 1],
+            [1, 1],
+            [1, 1, 1],
+            [1, 1],
+            [10]
+        ],
+    ]
+    ans = [
+        '◼◼◼◼◼◼◼◼◼◼',
+        '◼◻◻◻◻◻◻◻◻◼',
+        '◼◻◻◻◻◻◻◼◻◼',
+        '◼◻◼◼◼◻◻◻◻◼',
+        '◼◻◻◻◻◻◻◻◻◼',
+        '◼◻◼◼◼◻◻◻◻◼',
+        '◼◻◻◻◻◻◻◻◻◼',
+        '◼◻◼◼◼◻◻◻◻◼',
+        '◼◻◻◻◻◻◻◻◻◼',
+        '◼◼◼◼◼◼◼◼◼◼',
+    ]
+    ans = convert_to_marks(ans)
+    assert ans == slove(rule)
+
+
+def test_slove_medium():
+    rule = [
+        [
+            [1, 1],
+            [2],
+            [1, 2],
+            [1, 3, 2],
+            [6, 2],
+            [1, 3, 1, 1, 2, 1],
+            [2, 1, 1, 2, 2, 3],
+            [4, 7, 2],
+            [12],
+            [2, 11],
+            [9, 1, 2],
+            [3, 1, 2, 2, 2],
+            [2, 1, 2, 1, 1, 1],
+            [2, 1, 1, 2, 4],
+            [2, 1, 1, 6],
+            [1, 2, 2, 2, 1, 2],
+            [5, 1, 2, 3, 2],
+            [1, 2, 7, 1],
+            [2, 11, 1],
+            [2, 8, 5],
+            [2, 2, 6, 4],
+            [2, 2, 6],
+            [2, 2, 5],
+            [5, 3],
+            [3],
+        ],
+        [
+            [2, 1, 2],
+            [2, 2, 1, 6],
+            [1, 2, 12],
+            [3, 7, 2],
+            [2, 6, 1, 3],
+            [4, 3, 1, 4],
+            [1, 15],
+            [7, 3],
+            [2, 7, 4],
+            [4, 16],
+            [1, 4, 9],
+            [8, 7],
+            [4, 3, 3, 8],
+            [4, 2, 10],
+            [1, 3, 1, 3],
+            [1, 2, 2, 1, 3],
+            [2, 4, 3],
+            [2, 2, 2],
+            [1, 4, 2],
+            [2, 6],
+        ],
+    ]
+    ans = [
+        '◻◻◻◻◻◻◻◻◻◻◻◻◻◼◻◼◻◻◻◻',
+        '◻◻◻◻◻◻◻◻◻◻◻◻◻◼◼◻◻◻◻◻',
+        '◻◻◻◻◻◻◻◻◻◼◻◻◼◼◻◻◻◻◻◻',
+        '◻◻◻◻◻◼◻◻◼◼◼◻◼◼◻◻◻◻◻◻',
+        '◻◻◻◻◼◼◼◼◼◼◻◼◼◻◻◻◻◻◻◻',
+        '◼◻◻◼◼◼◻◼◻◼◻◼◼◻◻◻◻◼◻◻',
+        '◼◼◻◼◻◼◻◼◼◻◼◼◻◻◻◻◼◼◼◻',
+        '◻◼◼◼◼◻◼◼◼◼◼◼◼◻◻◼◼◻◻◻',
+        '◻◻◻◻◼◼◼◼◼◼◼◼◼◼◼◼◻◻◻◻',
+        '◼◼◻◻◼◼◼◼◼◼◼◼◼◼◼◻◻◻◻◻',
+        '◻◼◼◼◼◼◼◼◼◼◻◼◻◻◼◼◻◻◻◻',
+        '◻◻◼◼◼◻◼◻◼◼◻◼◼◻◻◼◼◻◻◻',
+        '◻◻◻◼◼◻◼◻◼◼◻◻◼◻◻◻◼◻◻◼',
+        '◻◻◼◼◻◻◼◻◻◼◻◻◼◼◻◻◼◼◼◼',
+        '◻◻◼◼◻◻◼◻◻◼◻◻◻◼◼◼◼◼◼◻',
+        '◼◻◼◼◻◼◼◻◻◼◼◻◻◼◻◻◻◻◼◼',
+        '◼◼◼◼◼◻◼◻◻◼◼◻◼◼◼◻◻◻◼◼',
+        '◻◻◼◻◻◻◼◼◻◼◼◼◼◼◼◼◻◻◻◼',
+        '◻◼◼◻◻◻◼◼◼◼◼◼◼◼◼◼◼◻◻◼',
+        '◻◼◼◻◻◻◼◼◼◼◼◼◼◼◻◼◼◼◼◼',
+        '◻◼◼◻◻◼◼◻◼◼◼◼◼◼◻◻◼◼◼◼',
+        '◻◼◼◻◻◼◼◻◼◼◼◼◼◼◻◻◻◻◻◻',
+        '◻◼◼◻◼◼◻◻◻◼◼◼◼◼◻◻◻◻◻◻',
+        '◻◼◼◼◼◼◻◻◻◻◼◼◼◻◻◻◻◻◻◻',
+        '◻◻◼◼◼◻◻◻◻◻◻◻◻◻◻◻◻◻◻◻',
+
+    ]
+    ans = convert_to_marks(ans)
+    assert ans == slove(rule)
